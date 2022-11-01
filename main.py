f37977f0f86c322632667e58cd1a9525bf0a362c
Langer, Jacek <jacek.langer@tu-darmstadt.de>
2022-11-01 15:31:44 +0100

    chore: progressbar

--- main.py
+++ main.py
@@ -29,6 +29,16 @@ def label(index):
     return LABELS[index + 8]
 
 
+def printBar(lbl, current, total, start):
+    cur = (current / total) * 100.
+    bars = "=" * int(cur / 10)
+    ws = " " * int(10 - (cur / 10))
+    bar = f"|{bars}>{ws}|"
+    time = dt.now() - start
+    # display progress
+    print("%5.2f \t%s\t %s testing %s" % (cur, bar, time, lbl), flush=True, end='\r')
+
+
 class NeuralNetwork:
 
     def __init__(self, who, whi):
@@ -37,12 +47,14 @@ class NeuralNetwork:
         self.whi = whi
 
     def evolve(self, gen, learningrate=.1):
+        total = gen * TOTAL_IMAGE_COUNT
+        start = dt.now()
         for i in range(gen):
-            for i in range(TOTAL_IMAGE_COUNT):
+            for j in range(TOTAL_IMAGE_COUNT):
                 r = ran.randint(0, TOTAL_IMAGE_COUNT - 1)
                 lbl = label(r)
                 inp = image(r)
-
+                printBar(lbl, (i + 1) * j, total, start)
                 # generate target array
                 tar = np.array([0] * 10, ndmin=2).T
                 tar[lbl] = 1
@@ -50,35 +62,33 @@ class NeuralNetwork:
                 self.train(tar, inp, learningrate)
 
     def eval(self, inp, lbl):
+        print("\n")
         # calculate the hidden layer
         hid = np.dot(self.whi, inp)
-        hid_out = sig(hid).reshape((89, 1))
+        hid_out = sig(hid)
         out = np.dot(self.who, hid_out)
         self.final_out = sig(out)
         print(f"expected {lbl}")
-        a = -1
-        index = -1
-        for i in out:
-            if a < out[i]:
-                a = out[i]
-                index = i
-
-        print(f"actual: {index}")
+        for i in range(10):
+            print("%d: %5.2f" % (i, out[i][0]))
 
     def train(self, target, inp, learningrate):
         # calculate the hidden layer
         lr = learningrate
         hid = np.dot(self.whi, inp)
-        hid_out = sig(hid).reshape((89, 1))
+        hid_out = sig(hid)
         out = np.dot(self.who, hid_out)
         self.final_out = sig(out)
         # calculate the individual error
         err = target - self.final_out
         hid_err = np.dot(self.who.T, err)
         # correct hidden output weigths
-        self.who += np.dot(lr * err * self.final_out * (1 - self.final_out), np.transpose(hid_out))
+        # todo(jack): assess this function, the values produced are totally of the charts.
+        output_error_m = err * self.final_out * (1 - self.final_out) * out
+        self.who += lr * np.dot(output_error_m, np.transpose(hid_out))
         # correct hidden input weights
-        self.whi += np.dot(lr * hid_err * hid_out * (1 - hid_out), np.transpose(inp))
+        hidden_error_m = hid_err * hid_out * (1 - hid_out) * hid
+        self.whi += lr * np.dot(hidden_error_m, np.transpose(inp))
 
 
 class NumpyArrayEncoder(JSONEncoder):
@@ -115,7 +125,7 @@ if __name__ == '__main__':
         whi=whi,
         who=who)
     start = dt.now()
-    net.evolve(gen=5)
+    net.evolve(gen=1)
     end = dt.now()
     r = ran.randint(0, TOTAL_IMAGE_COUNT - 1)
     net.eval(image(r), label(r))
